6e86f120950bf32c65356d771d4b68c48978e549
JoshB82 <jbryantjersey@hotmail.co.uk>
2020-06-02 19:31:36 +0100

    Initial structuring

---
+++ Physics-Simulator/Main Form.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Physics_Simulator
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu_Strip = new System.Windows.Forms.MenuStrip();
+            this.Canvas_Box = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas_Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Menu_Strip
+            // 
+            this.Menu_Strip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.Menu_Strip.Location = new System.Drawing.Point(0, 0);
+            this.Menu_Strip.Name = "Menu_Strip";
+            this.Menu_Strip.Size = new System.Drawing.Size(971, 24);
+            this.Menu_Strip.TabIndex = 0;
+            this.Menu_Strip.Text = "Menu_Strip";
+            // 
+            // Canvas_Box
+            // 
+            this.Canvas_Box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas_Box.Location = new System.Drawing.Point(0, 24);
+            this.Canvas_Box.Name = "Canvas_Box";
+            this.Canvas_Box.Size = new System.Drawing.Size(971, 768);
+            this.Canvas_Box.TabIndex = 1;
+            this.Canvas_Box.TabStop = false;
+            this.Canvas_Box.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Box_Paint);
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 792);
+            this.Controls.Add(this.Canvas_Box);
+            this.Controls.Add(this.Menu_Strip);
+            this.Name = "Main_Form";
+            this.Text = "Physics Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas_Box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip Menu_Strip;
+        private System.Windows.Forms.PictureBox Canvas_Box;
+    }
+}
+
---
+++ Physics-Simulator/Main Form.cs
@@ -0,0 +1,36 @@
+﻿using _3D_Engine;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Physics_Simulator
+{
+    public partial class Main_Form : Form
+    {
+        private Scene scene;
+
+        public Main_Form()
+        {
+            // Create form
+            InitializeComponent();
+
+            // Create scene
+            scene = new Scene(Canvas_Box, Canvas_Box.Width, Canvas_Box.Height);
+
+            // Create origin and axes
+            scene.Create_Origin();
+            scene.Create_Axes();
+
+            // Create camera
+            double camera_width = Canvas_Box.Width / 10;
+            double camera_height = Canvas_Box.Height / 10;
+
+            Perspective_Camera camera = new Perspective_Camera(new Vector3D(0, 0, 100), scene.Shape_List[0].Render_Mesh, Vector3D.Unit_Y, camera_width, camera_height, 10, 750);
+            scene.Render_Camera = camera;
+            
+            // Render the scene
+            scene.Render();
+        }
+
+        private void Canvas_Box_Paint(object sender, PaintEventArgs e) => e.Graphics.DrawImageUnscaled(scene.Canvas, Point.Empty);
+    }
+}

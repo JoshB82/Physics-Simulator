39b142a54f48d7ea8a1ae1609d36bddac97556cf
JoshB82 <jbryantjersey@hotmail.co.uk>
2020-06-03 20:09:19 +0100

    Implemented loop

--- Physics-Simulator/Main Form.Designer.cs
+++ Physics-Simulator/Main Form.Designer.cs
@@ -38,7 +38,8 @@
             this.Menu_Strip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.Menu_Strip.Location = new System.Drawing.Point(0, 0);
             this.Menu_Strip.Name = "Menu_Strip";
-            this.Menu_Strip.Size = new System.Drawing.Size(971, 24);
+            this.Menu_Strip.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.Menu_Strip.Size = new System.Drawing.Size(777, 24);
             this.Menu_Strip.TabIndex = 0;
             this.Menu_Strip.Text = "Menu_Strip";
             // 
@@ -46,19 +47,21 @@
             // 
             this.Canvas_Box.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Canvas_Box.Location = new System.Drawing.Point(0, 24);
+            this.Canvas_Box.Margin = new System.Windows.Forms.Padding(2);
             this.Canvas_Box.Name = "Canvas_Box";
-            this.Canvas_Box.Size = new System.Drawing.Size(971, 768);
+            this.Canvas_Box.Size = new System.Drawing.Size(777, 610);
             this.Canvas_Box.TabIndex = 1;
             this.Canvas_Box.TabStop = false;
             this.Canvas_Box.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Box_Paint);
             // 
             // Main_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(971, 792);
+            this.ClientSize = new System.Drawing.Size(777, 634);
             this.Controls.Add(this.Canvas_Box);
             this.Controls.Add(this.Menu_Strip);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Main_Form";
             this.Text = "Physics Simulator";
             ((System.ComponentModel.ISupportInitialize)(this.Canvas_Box)).EndInit();
--- Physics-Simulator/Main Form.cs
+++ Physics-Simulator/Main Form.cs
@@ -1,5 +1,7 @@
 ﻿using _3D_Engine;
+using System.Diagnostics;
 using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Physics_Simulator
@@ -7,6 +9,7 @@ namespace Physics_Simulator
     public partial class Main_Form : Form
     {
         private Scene scene;
+        private bool running = true;
 
         public Main_Form()
         {
@@ -26,11 +29,65 @@ namespace Physics_Simulator
 
             Perspective_Camera camera = new Perspective_Camera(new Vector3D(0, 0, 100), scene.Shape_List[0].Render_Mesh, Vector3D.Unit_Y, camera_width, camera_height, 10, 750);
             scene.Render_Camera = camera;
+
+            // Start loop
+            Thread thread = new Thread(Loop);
+            thread.Start();
+            thread.IsBackground = true;
+        }
+
+        private void Loop()
+        {
+            const int max_frames_per_second = 60;
+            const int max_updates_per_second = 60;
+            const long frame_minimum_time = 1000 / max_frames_per_second;
+            const long update_minimum_time = 1000 / max_updates_per_second;
+
+            int no_frames = 0, no_updates = 0, timer = 1;
+
+            long now_time, delta_time;
+            long frame_time = 0, update_time = 0;
             
-            // Render the scene
-            scene.Render();
+            Stopwatch sw = Stopwatch.StartNew();
+            long start_time = sw.ElapsedMilliseconds;
+            
+            while (running)
+            {
+                now_time = sw.ElapsedMilliseconds;
+                delta_time = now_time - start_time;
+                start_time = now_time;
+
+                frame_time += delta_time;
+                update_time += delta_time;
+
+                if (frame_time >= frame_minimum_time)
+                {
+                    scene.Render();
+                    no_frames++;
+                    frame_time -= frame_minimum_time;
+                }
+
+                if (update_time >= update_minimum_time)
+                {
+                    Update_Position(update_time);
+                    no_updates++;
+                    update_time -= update_minimum_time;
+                }
+
+                if (now_time >= 1000 * timer)
+                {
+                    Invoke((MethodInvoker)delegate { Text = $"Physics Simulator - FPS: {no_frames}, UPS: {no_updates}"; }); // ?
+                    no_frames = 0; no_updates = 0;
+                    timer += 1;
+                }
+            }
         }
 
         private void Canvas_Box_Paint(object sender, PaintEventArgs e) => e.Graphics.DrawImageUnscaled(scene.Canvas, Point.Empty);
+
+        private void Update_Position(long time)
+        {
+
+        }
     }
 }
